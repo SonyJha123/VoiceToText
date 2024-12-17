a4cb251470e01235f69406e89a0af0b5a2e99cef
Sony_jha <sony@sleekitech.com>
2024-12-17 12:56:52 +0530

    abcd

--- server/mondodb.js
+++ server/mondodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
   
   const connectdb = async (req, res) => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/Voice-Contact")
+        await mongoose.connect("mongodb://0.0.0.0:27017/Voice-Contact")
 
         console.log("Database connected successfully");
         
